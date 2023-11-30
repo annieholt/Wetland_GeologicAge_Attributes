b47d382637401c11da1b92998db459d997f99bc6
annieholt <holtannie@gmail.com>
2023-11-29 20:20:37 -0800

    updating figures for correlations

--- libs/correlation_analysis.R
+++ libs/correlation_analysis.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(sf)
 library(ggplot2)
+library(corrr)
 
 # Gages II signature data appending
 
@@ -41,12 +42,14 @@ sigs = sigs_c_subset %>%
 
 sigs_c_2 = sigs_c %>% 
   select(gauge_id, BFI, Recession_a_Seasonality, BaseflowRecessionK, First_Recession_Slope, Mid_Recession_Slope,
-         MRC_num_segments, Spearmans_rho, VariabilityIndex) %>% 
+         MRC_num_segments, Spearmans_rho, VariabilityIndex, RecessionParameters_a, RecessionParameters_b, RecessionParameters_c) %>% 
   mutate(datasource = 'CAMELS')
 sigs_g_2 = sigs_g %>% 
   select(gauge_id, BFI, Recession_a_Seasonality, BaseflowRecessionK, First_Recession_Slope, Mid_Recession_Slope,
-         MRC_num_segments, Spearmans_rho, VariabilityIndex) %>% 
-  mutate(datasource = 'GagesII')
+         MRC_num_segments, Spearmans_rho, VariabilityIndex, RecessionParameters_1, RecessionParameters_2, RecessionParameters_3) %>% 
+  mutate(datasource = 'GagesII') %>% 
+  rename(RecessionParameters_a = RecessionParameters_1, RecessionParameters_b = RecessionParameters_2,
+         RecessionParameters_c = RecessionParameters_3)
 
 
 sigs_2 = sigs_c_2 %>% 
@@ -68,7 +71,8 @@ nwi_metrics = nwi_g %>%
   bind_rows(nwi_c) %>% 
   left_join(giws, by = 'gauge_id') %>% 
   # some places have zero isolated wetlands, so replace NAs with zero since shapefiles weren't returned for those
-  mutate(area_frac = ifelse(is.na(area_frac), 0, area_frac))
+  mutate(area_frac = ifelse(is.na(area_frac), 0, area_frac)) %>% 
+  mutate(fresh_no_giw = fresh + lake - area_frac)
 
 #### FINAL DATASET FOR PLOTTING AND ANALYSIS ####
 
@@ -83,6 +87,148 @@ nwi_sigs = nwi_metrics %>%
   arrange(desc(area_frac))
 
 
+nwi_sigs_2 = nwi_metrics %>% 
+  left_join(sigs_2, by = c('gauge_id')) %>% 
+  drop_na() %>% 
+  mutate(fresh_total = fresh + lake) %>% 
+  arrange(desc(area_frac))
+
+nwi_sigs_camels = nwi_metrics %>% 
+  select(gauge_id,area_frac, fresh_no_giw, geometry) %>% 
+  left_join(sigs_c, by = c('gauge_id')) %>% 
+  filter(!is.na(gauge_lat))
+
+
+#### wetland correlations #### 
+
+ggplot(nwi_sigs_2, aes(x = fresh_no_giw, y = area_frac)) +
+  geom_point(size = 4, color = 'black', shape = 21) +  # Increase the size of dots
+  # geom_smooth(method = "loess", se = FALSE, color = "blue") +  # Add a smoothed line
+  # ggtitle(paste("Spearman's Rank Correlation =", round(cor_result$estimate, 2),
+  #               "p-value =", round(cor_result$p.value, 4))) +
+  xlab("Wetland Area Fraction") +
+  ylab("GIWs") +
+  theme_minimal()+
+  theme(
+    axis.title.x = element_text(margin = margin(t = 10)),  # Increase space below X-axis label
+    axis.title.y = element_text(margin = margin(r = 10)),  # Increase space to the right of Y-axis label
+    plot.margin = margin(20, 20, 20, 20)  # Adjust overall plot margin
+  )
+
+# ggsave("E:/SDSU_GEOG/Thesis/Data/Signatures/Figures/BaseflowRecessionK_fresh.png", width = 4, height = 3, dpi = 300,bg = "white")
+
+
+cor_wet <- cor.test(nwi_sigs_2$fresh_no_giw, nwi_sigs_2$area_frac, method = "spearman", exact = FALSE)
+
+# Print the correlation coefficient and p-value
+cat("Spearman rank correlation coefficient:", cor_wet$estimate, "\n")
+cat("P-value:", cor_wet$p.value, "\n")
+
+
+# cor_spearman <- cor(nwi_sigs_2$fresh_no_giw, nwi_sigs_2$area_frac, method = "spearman")
+# 
+# # Scatter plot
+# plot(nwi_sigs_2$fresh_no_giw, nwi_sigs_2$area_frac, main = "Scatter Plot with Spearman Correlation", 
+#      xlab = "Variable 1", ylab = "Variable 2", pch = 16, col = "blue")
+# text(x = max(nwi_sigs_2$fresh_no_giw), y = max(nwi_sigs_2$area_frac), 
+#      label = paste("Spearman Correlation =", round(cor_spearman, 2)), pos = 4)
+
+
+corr_test = nwi_sigs_2 %>% 
+  select(fresh_no_giw, BFI, Recession_a_Seasonality, BaseflowRecessionK, First_Recession_Slope, Mid_Recession_Slope,
+         MRC_num_segments, VariabilityIndex, RecessionParameters_a, RecessionParameters_b, RecessionParameters_c) %>% 
+  as.data.frame() %>% 
+  correlate(method = "spearman") %>% 
+  focus(fresh_no_giw) %>% 
+  mutate(term_2 = factor(term, levels = term[order(fresh_no_giw)]))
+
+
+
+# ggplot(corr_test, aes(x = term_2, y = fresh_no_giw)) +
+#   geom_bar(stat = "identity", fill = "skyblue", color = "black") +
+#   labs(
+#     y = "Spearman Rank Correlation Coefficient",
+#     x = "Hydrologic Signature"
+#   ) +
+#   theme_minimal() +                    # Use a minimal theme
+#   theme(
+#     panel.grid = element_blank(),     # Remove background grid lines
+#     text = element_text(size = 12),   # Increase text (axis labels, title) size
+#     axis.title = element_text(size = 14)  # Increase axis title size
+#   ) +
+#   coord_cartesian(ylim = c(-0.25, 0.25))
+
+
+# Create ggplot with colored and sized dots
+ggplot(corr_test, aes(x = term, y = 1, color = fresh_no_giw, size = abs(fresh_no_giw))) +
+  geom_point() +
+  scale_color_gradient2(low = "red", mid = "white", high = "blue", 
+                        midpoint = mean(corr_test$fresh_no_giw), name = "Spearman's Rho",
+                        limits = c(-0.3, 0.3)) +
+  scale_size(range = c(2, 10)) +  # Adjust the overall size scale
+  labs(
+    y = NULL,  # No y-axis label
+    x = NULL,
+  ) +
+  ggtitle("Correlations with Non-Isolated Wetland Area Fraction") +  # Add plot title
+  theme_minimal() +                    # Use a minimal theme
+  theme(
+    plot.title = element_text(size=12),
+    text = element_text(size = 12),   # Increase text (axis labels, title) size
+    axis.title = element_text(size = 14),  # Increase axis title sizehttp://127.0.0.1:42413/graphics/plot_zoom_png?width=619&height=258
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Rotate x-axis labels diagonally
+    axis.text.y = element_blank(),  # Remove y-axis values
+    legend.position = "left",  # Move legend to the left side
+    legend.title = element_text(size = 12),
+    legend.text = element_text(size = 12),  # Adjust legend text size
+    legend.key.size = unit(1, "lines")  # Adjust the size of the legend color key
+  ) +
+  guides(size = FALSE)+
+  coord_cartesian(ylim = c(1, 1))
+
+
+# ggsave("E:/SDSU_GEOG/Thesis/Data/Signatures/Figures/giw_spearmans.png", width = 6, height = 3, dpi = 300,bg = "white")
+
+
+
+
+#### camels correlations ####
+
+corr_test_camels = nwi_sigs_camels %>% 
+  select(-geometry, -gauge_id, -gauge_lon, -gauge_lat, -fresh_no_giw) %>% 
+  as.data.frame() %>% 
+  correlate(method = "spearman") %>% 
+  focus(area_frac) %>% 
+  mutate(term_2 = factor(term, levels = term[order(area_frac)]))
+
+ggplot(corr_test_camels, aes(x = term, y = 1, color = area_frac, size = abs(area_frac))) +
+  geom_point() +
+  scale_color_gradient2(low = "red", mid = "white", high = "blue", 
+                        midpoint = mean(corr_test_camels$area_frac), name = "Spearman's Rho",
+                        limits = c(-0.24, 0.2)) +
+  scale_size(range = c(2, 14)) +  # Adjust the overall size scale
+  labs(
+    y = NULL,  # No y-axis label
+    x = NULL,
+  ) +
+  ggtitle("Correlations with Isolated Wetland Area Fraction (CAMELS only)") +  # Add plot title
+  theme_minimal() +                    # Use a minimal theme
+  theme(
+    plot.title = element_text(size=12),
+    text = element_text(size = 12),   # Increase text (axis labels, title) size
+    axis.title = element_text(size = 14),  # Increase axis title sizehttp://127.0.0.1:42413/graphics/plot_zoom_png?width=619&height=258
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Rotate x-axis labels diagonally
+    axis.text.y = element_blank(),  # Remove y-axis values
+    legend.position = "left",  # Move legend to the left side
+    legend.title = element_text(size = 12),
+    legend.text = element_text(size = 12),  # Adjust legend text size
+    legend.key.size = unit(1, "lines")  # Adjust the size of the legend color key
+  ) +
+  guides(size = FALSE)+
+  coord_cartesian(ylim = c(1, 1))
+
+# ggsave("E:/SDSU_GEOG/Thesis/Data/Signatures/Figures/giw_camels_spearmans.png", width = 8, height = 3, dpi = 300,bg = "white")
+
 
 
 #### trying some figures ####
@@ -95,12 +241,12 @@ centroids_sf <- st_centroid(nwi_sigs)
 # Plot the map with polygons represented as dots, colored by the variable
 ggplot() +
   geom_sf(data = conus, color = "white", fill = "grey") +  # Plot polygon boundaries, color by variable
-  geom_sf(data = centroids_sf, aes(fill = area_frac), shape = 21, color = "darkgrey", size = 4, alpha = 1) +  # Plot centroids as dots, color by variable
+  geom_sf(data = centroids_sf, aes(fill = fresh_no_giw), shape = 21, color = "darkgrey", size = 4, alpha = 1) +  # Plot centroids as dots, color by variable
   # scale_fill_distiller(direction=1)+
   # scale_fill_viridis_c()+
   scale_fill_distiller(palette = "YlGnBu", direction = 1, name = NULL)+
   # scale_fill_gradient(low = "white", high = "blue4") +  # Set color scale
-  ggtitle("Fresh Total") +
+  ggtitle("Non-Isolated Wetland Area Fraction") +
   theme_void()+
   theme(
     plot.title = element_text(size = 15, hjust = 0.5),  # Adjust title size and centering
@@ -110,27 +256,22 @@ ggplot() +
     legend.margin = margin(r = 10)
   )
 
-# ggsave("E:/SDSU_GEOG/Thesis/Data/Signatures/Figures/giws.png", width = 11, height = 6, dpi = 300,bg = "white")
+# ggsave("E:/SDSU_GEOG/Thesis/Data/Signatures/Figures/nwi_fresh_no_giw.png", width = 11, height = 6, dpi = 300,bg = "white")
 
-
-nwi_sigs_2 = nwi_metrics %>% 
-  left_join(sigs_2, by = c('gauge_id')) %>% 
-  drop_na() %>% 
-  arrange(desc(area_frac))
 
 
 nwi_sigs_categories = nwi_sigs_2 %>% 
-  mutate(fresh_total = fresh + lake) %>% 
-  mutate(fresh_category = case_when(fresh < 0.2 ~ 'class 1',
-                                    fresh < 0.4 & fresh >= 0.2 ~ 'class 2',
+  # mutate(fresh_total = fresh + lake) %>% 
+  mutate(fresh_category = case_when(fresh_no_giw < 0.1 ~ 'class 1',
+                                    fresh_no_giw < 0.3 & fresh_no_giw >= 0.1 ~ 'class 2',
                                     # fresh < 0.6 & fresh >= 0.4 ~ 'class 3',
                                     TRUE ~ 'class 3')) %>% 
   # mutate(total_fresh_category = case_when(fresh_total < 0.2 ~ 'class 1',
   #                                         fresh_total < 0.4 & fresh_total >= 0.2 ~ 'class 2',
   #                                   # fresh < 0.6 & fresh >= 0.4 ~ 'class 3',
   #                                   TRUE ~ 'class 3')) %>% 
-  mutate(giw_category = case_when(area_frac < 0.04 ~ 'class 1',
-                                  area_frac < 0.08 & area_frac >= 0.04 ~ 'class 2',
+  mutate(giw_category = case_when(area_frac < 0.01 ~ 'class 1',
+                                  area_frac < 0.05 & area_frac >= 0.01 ~ 'class 2',
                                   # area_frac < 0.12 & area_frac >= 0.08 ~ 'class 3',
                                   TRUE ~ 'class 3')) %>% 
   st_drop_geometry() %>% 
@@ -139,18 +280,22 @@ nwi_sigs_categories = nwi_sigs_2 %>%
   
   # # adjusting values based on McMillan et al. 2022 (based on exected distribution of GW signatures??)
   mutate(BaseflowRecessionK = ifelse(BaseflowRecessionK > 0.6, NA, BaseflowRecessionK)) %>%
-  mutate(Recession_a_Seasonality = ifelse(Recession_a_Seasonality > 6, NA, Recession_a_Seasonality)) %>% 
-  mutate(VariabilityIndex = ifelse(VariabilityIndex > 1, NA, VariabilityIndex)) %>% 
-  mutate(Mid_Recession_Slope = ifelse(Mid_Recession_Slope > 0.6, NA, Mid_Recession_Slope)) %>% 
-  mutate(First_Recession_Slope = ifelse(First_Recession_Slope > 1.5, NA, First_Recession_Slope))
+  mutate(Recession_a_Seasonality = ifelse(Recession_a_Seasonality > 6, NA, Recession_a_Seasonality)) %>%
+  # mutate(VariabilityIndex = ifelse(VariabilityIndex > 1, NA, VariabilityIndex)) %>%
+  mutate(Mid_Recession_Slope = ifelse(Mid_Recession_Slope > 0.8, NA, Mid_Recession_Slope)) %>%
+  mutate(First_Recession_Slope = ifelse(First_Recession_Slope > 2, NA, First_Recession_Slope)) %>% 
+  mutate(RecessionParameters_b = ifelse(RecessionParameters_b > 8, NA, RecessionParameters_b)) %>% 
+  mutate(RecessionParameters_c = ifelse(RecessionParameters_c > 100, NA, RecessionParameters_c))
 
 
 
 nwi_sigs_categories_long = nwi_sigs_categories %>% 
   # filter(NA_L1KEY == "5  NORTHERN FORESTS") %>% 
   # select(giw_category, BFI, Recession_a_Seasonality, BaseflowRecessionK) %>% 
-  select(fresh_category, BFI, Recession_a_Seasonality, BaseflowRecessionK, First_Recession_Slope, Mid_Recession_Slope, VariabilityIndex) %>%
-  pivot_longer(!fresh_category, names_to = "signature")
+  # select(fresh_category, BFI, Recession_a_Seasonality, BaseflowRecessionK, First_Recession_Slope, Mid_Recession_Slope, VariabilityIndex) %>%
+  select(giw_category, BFI, BaseflowRecessionK, VariabilityIndex, First_Recession_Slope, RecessionParameters_b,
+         RecessionParameters_c) %>%
+  pivot_longer(!giw_category, names_to = "signature")
   
 
 # ggplot(nwi_sigs_categories, aes(x = fresh_category, y = BFI, fill = fresh_category)) +
@@ -166,14 +311,14 @@ ggplot(nwi_sigs_categories_long, aes(x = fresh_category, y = value, fill = fresh
   geom_boxplot() +
   facet_wrap(~ signature, scales = "free_y", ncol = 3) +
   scale_fill_manual(values = c("class 1" = "yellow", "class 2" = "green", "class 3" = "blue"),
-                     name = str_wrap("Percent Wetlands", width = 16),
+                     name = str_wrap("Percent Non-Isolated Wetlands", width = 16),
                      labels = c("0-20%", "20-40%", "40-80%")) +
   # ggtitle("Boxplots for Variable X by Different Y Variables") +
   xlab(NULL) +  # Remove X-axis label
   ylab(NULL) +  # Remove Y-axis label
   scale_x_discrete(
     breaks = c("class 1", "class 2", "class 3"),
-    labels = c("0-20%", "20-40%", "40-80%")
+    labels = c("0-10%", "10-30%", "> 30%")
   ) +
   theme_minimal() +
   theme(
@@ -198,12 +343,12 @@ ggplot(nwi_sigs_categories_long, aes(x = fresh_category, y = value, fill = fresh
 
 
 # Creating a ggplot with overlapping histograms for different categories
-ggplot(nwi_sigs_categories_long, aes(x = value, color = fresh_category)) +
+ggplot(nwi_sigs_categories_long, aes(x = value, color = giw_category)) +
   geom_density(lwd = 2, alpha = 0.7) +  # Adjust line thickness with lwd and transparency with alpha
   facet_wrap(~ signature, scales = "free", ncol = 2, strip.position = "bottom") +
   scale_color_manual(values = c("class 1" = "yellow", "class 2" = "green", "class 3" = "blue"),
                      name = str_wrap("Percent Isolated Wetlands", width = 16),
-                     labels = c("0-4%", "4-8%", "8-16%")) +  # Specify specific colors
+                     labels = c("0-1%", "1-5%", "> 5%")) +  # Specify specific colors
   # ggtitle("Kernel Density Plot with Separate Lines for Different Categories") +
   xlab(NULL) +
   ylab(NULL) +
@@ -220,7 +365,8 @@ ggplot(nwi_sigs_categories_long, aes(x = value, color = fresh_category)) +
     legend.text = element_text(size = 16),  # Increase legend text size
     legend.title = element_text(size = 18)  # Increase legend title size
   )
-# ggsave("E:/SDSU_GEOG/Thesis/Data/Signatures/Figures/sig_distribution_giw_extended.png", width = 11, height = 8, dpi = 300,bg = "white")
+
+ggsave("E:/SDSU_GEOG/Thesis/Data/Signatures/Figures/sig_distribution_giw_extended_2.png", width = 11, height = 8, dpi = 300,bg = "white")
 
 
 
@@ -228,23 +374,23 @@ ggplot(nwi_sigs_categories_long, aes(x = value, color = fresh_category)) +
 #### Statistical testing ####
 
 nwi_sigs_test <- nwi_sigs_categories %>% 
-  filter(NA_L1KEY == "5  NORTHERN FORESTS") %>% 
-  select(gauge_id, fresh, First_Recession_Slope) %>% 
+  filter(NA_L1KEY == "8  EASTERN TEMPERATE FORESTS") %>%
+  select(gauge_id, fresh, VariabilityIndex) %>%
   drop_na()
-cor_result <- cor.test(nwi_sigs_test$fresh, nwi_sigs_test$First_Recession_Slope, method = "spearman", exact = FALSE)
+cor_result <- cor.test(nwi_sigs_test$fresh, nwi_sigs_test$VariabilityIndex, method = "spearman", exact = FALSE)
 
 # Print the correlation coefficient and p-value
 cat("Spearman rank correlation coefficient:", cor_result$estimate, "\n")
 cat("P-value:", cor_result$p.value, "\n")
 
 
-ggplot(nwi_sigs_test, aes(x = fresh, y = First_Recession_Slope)) +
+ggplot(nwi_sigs_test, aes(x = fresh, y = BaseflowRecessionK)) +
   geom_point(size = 4, color = 'black', shape = 21) +  # Increase the size of dots
   # geom_smooth(method = "loess", se = FALSE, color = "blue") +  # Add a smoothed line
   # ggtitle(paste("Spearman's Rank Correlation =", round(cor_result$estimate, 2),
   #               "p-value =", round(cor_result$p.value, 4))) +
-  xlab("GIW Area Fraction") +
-  ylab("BFI") +
+  xlab("Wetland Area Fraction") +
+  ylab("BaseflowRecessionK") +
   theme_minimal()+
   theme(
     axis.title.x = element_text(margin = margin(t = 10)),  # Increase space below X-axis label
@@ -252,7 +398,7 @@ ggplot(nwi_sigs_test, aes(x = fresh, y = First_Recession_Slope)) +
     plot.margin = margin(20, 20, 20, 20)  # Adjust overall plot margin
   )
 
-# ggsave("E:/SDSU_GEOG/Thesis/Data/Signatures/Figures/BFI_fresh.png", width = 4, height = 3, dpi = 300,bg = "white")
+ggsave("E:/SDSU_GEOG/Thesis/Data/Signatures/Figures/BaseflowRecessionK_fresh.png", width = 4, height = 3, dpi = 300,bg = "white")
 
 
 
