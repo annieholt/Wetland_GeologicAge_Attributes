d146317548d1515eec6e6385092ddd1d1b6c172f
annieholt <holtannie@gmail.com>
2024-03-25 16:56:20 -0700

    re-run geology correlation, but facet plotting

--- libs/geologic_age_explore.R
+++ libs/geologic_age_explore.R
@@ -29,6 +29,21 @@ library(broom)
 #   )
 
 
+
+##### HYSETS data ####
+
+# camels = st_read('E:/SDSU_GEOG/Thesis/Data/Caravan/shapefiles/camels/camels_basin_shapes_nad83.shp')
+# camels_df = camels %>% as.data.frame() %>% select(gauge_id, gauge_id_2) %>% rename(gauge_id_camels = gauge_id)
+# 
+# hysets = st_read('E:/SDSU_GEOG/Thesis/Data/Caravan/shapefiles/hysets/hysets_basin_shapes_conus.shp')
+# 
+# hysets_2 = hysets %>% 
+#   left_join(camels_df, by = c("gauge_id" = "gauge_id_2")) %>% 
+#   filter(is.na(gauge_id_camels)) %>% 
+#   select(gauge_id, geometry)
+# 
+# st_write(hysets_2, "E:/SDSU_GEOG/Thesis/Data/Caravan/shapefiles/hysets/hysets_basin_shapes_nocamels.shp")
+
 ##### SOME CORRELATION ANALYSIS and PLOTTING ####
 
 
@@ -38,17 +53,18 @@ sigs_c = read.csv('E:/SDSU_GEOG/Thesis/Data/Signatures/sigs_camels.csv', colClas
 
 # signatures, no precip data needed
 sigs_c_2 = sigs_c %>% 
-  select(gauge_id, BFI, Recession_a_Seasonality, BaseflowRecessionK, First_Recession_Slope, Mid_Recession_Slope,
-         MRC_num_segments, Spearmans_rho, VariabilityIndex, RecessionParameters_a, RecessionParameters_b, RecessionParameters_c) %>% 
-  mutate(datasource = 'CAMELS')
+  select(gauge_id,EventRR, TotalRR, RR_Seasonality, Recession_a_Seasonality, AverageStorage, RecessionParameters_a,
+         RecessionParameters_b, RecessionParameters_c, First_Recession_Slope, Mid_Recession_Slope, EventRR_TotalRR_ratio,
+         VariabilityIndex, BaseflowRecessionK, BFI) %>% 
+  rename(RecessionParameters_T0 = RecessionParameters_c)
 
 # groundwater signature set
 # removed Storage Fraction as McMillan et al. 2022 found unreliable
-sigs_c_3 = sigs_c %>% 
-  select(gauge_id, TotalRR, RR_Seasonality, EventRR, Recession_a_Seasonality,
-         AverageStorage, RecessionParameters_a, RecessionParameters_b, RecessionParameters_c, MRC_num_segments,
-         First_Recession_Slope, Mid_Recession_Slope, Spearmans_rho, EventRR_TotalRR_ratio,
-         VariabilityIndex, BFI, BaseflowRecessionK)
+# sigs_c_3 = sigs_c %>% 
+#   select(gauge_id, TotalRR, RR_Seasonality, EventRR, Recession_a_Seasonality,
+#          AverageStorage, RecessionParameters_a, RecessionParameters_b, RecessionParameters_c, MRC_num_segments,
+#          First_Recession_Slope, Mid_Recession_Slope, Spearmans_rho, EventRR_TotalRR_ratio,
+#          VariabilityIndex, BFI, BaseflowRecessionK)
 
 
 #### PREP GEOLOGY AGE METRICS DATA ###
@@ -62,20 +78,26 @@ geol_c_av = st_read('E:/SDSU_GEOG/Thesis/Data/Geology_outputs/Shapefiles/sgmc_ca
 #### FINAL DATASET FOR PLOTTING AND ANALYSIS ####
 
 
-geol_sigs = sigs_c_3 %>% 
+geol_sigs = sigs_c_2 %>% 
   left_join(geol_c, by = c('gauge_id')) %>% 
   drop_na()
 # why are there NAs???
 
-geol_sigs_av = sigs_c_3 %>% 
+geol_sigs_av = sigs_c_2 %>% 
   left_join(geol_c_av, by = c('gauge_id')) %>% 
   drop_na()
 # why are there NAs???
 
+
+geol_sigs = sigs_c_2 %>% 
+  left_join(geol_c %>% select(-shed_area, -major_lith, -lith_area_), by = c('gauge_id')) %>% 
+  left_join(geol_c_av %>% select(gauge_id, av_age_w), by = c('gauge_id', 'geometry'))
+
+
 #### SIGNATURE DISTRIBUTIONS AND PERCENTILES ####
 # sanity checking signature calculations
 
-sigs_c_3_long = sigs_c_3 %>% 
+sigs_c_3_long = sigs_c_2 %>% 
   pivot_longer(-gauge_id, names_to = "signature")
 
 # Creating a ggplot of distributions
@@ -112,6 +134,16 @@ cor_results <- corr_test %>%
   group_by(variable) %>%
   do(tidy(cor.test(.$value, .$av_age, method = "spearman"))) %>%
   select(variable, estimate, p.value)
+
+
+
+
+
+
+
+
+
+
 
 
 # Create ggplot with colored and sized dots
@@ -160,7 +192,7 @@ ggplot() +
   # scale_fill_viridis_c()+
   scale_fill_distiller(palette = "YlOrRd", direction = 1, name = NULL)+
   # scale_fill_gradient(low = "white", high = "blue4") +  # Set color scale
-  ggtitle("Average Geologic Age of Major Lithology (Ma)") +
+  ggtitle("Geologic Age of Major Lithology (Ma)") +
   theme_void()+
   theme(
     plot.title = element_text(size = 30, hjust = 0.5),  # Adjust title size and centering
@@ -171,7 +203,7 @@ ggplot() +
   )
 
 
-# ggsave("E:/SDSU_GEOG/Thesis/Data/Signatures/figures_geology/geol_age_lith.png", width = 11, height = 6, dpi = 300,bg = "white")
+ggsave("E:/SDSU_GEOG/Thesis/Data/Signatures/figures_final/geol_age_lith_camels.png", width = 11, height = 6, dpi = 300,bg = "white")
 
 # and major lithologies plot 
 # ggplot() +
@@ -188,4 +220,63 @@ ggplot() +
 #   )
 
 
+#### CORRELATIONS ####
+
+corr_av = geol_sigs %>% 
+  select(av_age, EventRR, TotalRR, RR_Seasonality, Recession_a_Seasonality, AverageStorage, RecessionParameters_a,
+         RecessionParameters_b, RecessionParameters_T0, First_Recession_Slope, Mid_Recession_Slope, EventRR_TotalRR_ratio,
+         VariabilityIndex, BaseflowRecessionK, BFI) %>% 
+  as.data.frame() %>% 
+  correlate(method = "spearman") %>% 
+  focus(av_age)
+
+corr_av_w = geol_sigs %>% 
+  select(av_age_w, EventRR, TotalRR, RR_Seasonality, Recession_a_Seasonality, AverageStorage, RecessionParameters_a,
+         RecessionParameters_b, RecessionParameters_T0, First_Recession_Slope, Mid_Recession_Slope, EventRR_TotalRR_ratio,
+         VariabilityIndex, BaseflowRecessionK, BFI) %>% 
+  as.data.frame() %>% 
+  correlate(method = "spearman") %>% 
+  focus(av_age_w)
+
+# prep for panel plotting
+corr_sigs_geol = corr_av %>% 
+  left_join(corr_av_w, by = "term") %>% 
+  gather(key = "variable", value = "value", av_age, av_age_w)
+
+
+# Define custom labels for facet_wrap
+geol_labels <- c(
+  "av_age" = "Average Geologic Age (Ma) of Major Lithology",
+  "av_age_w" = "Average Geologic Age (Ma)"
+)
+
+ggplot(corr_sigs_geol, aes(x = term, y = 1, color = value, size = abs(value))) +
+  geom_point() +
+  scale_color_gradient2(low = "red", mid = "grey", high = "blue", 
+                        midpoint = mean(corr_sigs_geol$value), name = "Spearman's Rho",
+                        limits = c(-0.3, 0.3)) +
+  scale_size(range = c(6, 16)) +  # Adjust the overall size scale
+  labs(
+    y = NULL,  # No y-axis label
+    x = NULL,
+  ) +
+  # ggtitle("Correlations with Isolated Wetland Area Fraction") +
+  theme_minimal() +                    # Use a minimal theme
+  theme(
+    plot.title = element_text(size=24),
+    text = element_text(size = 20),   # Increase text (axis labels, title) size
+    axis.title = element_text(size = 14),  # Increase axis title sizehttp://127.0.0.1:42413/graphics/plot_zoom_png?width=619&height=258
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Rotate x-axis labels diagonally
+    axis.text.y = element_blank(),  # Remove y-axis values
+    legend.position = "left",  # Move legend to the left side
+    legend.title = element_text(size = 20),
+    legend.text = element_text(size = 20),  # Adjust legend text size
+    legend.key.size = unit(2, "lines")  # Adjust the size of the legend color key
+  ) +
+  guides(size = FALSE)+
+  # coord_cartesian(ylim = c(1, 1))
+  facet_wrap(~ variable, scales = "free_y", nrow = 2, labeller = as_labeller(geol_labels))
+
+
+# ggsave("E:/SDSU_GEOG/Thesis/Data/Signatures/figures_final/sigs_geol_camels.png", width = 10.5, height = 6, dpi = 300,bg = "white")
 
